a4c2d3d3c6b9e633e37a477047a44a49884a9aef
safetymax <31389735+safetymax@users.noreply.github.com>
2025-11-15 05:15:49 +0100

    fixed animation script

--- Assets/Scripts/UI/simpleAnimator.cs
+++ Assets/Scripts/UI/simpleAnimator.cs
@@ -4,16 +4,17 @@ public class simpleAnimator : MonoBehaviour
 {
     public void Start()
     {
-        //playAnim(new Vector2(0, -500), Vector2.zero, 2f, AnimationType.EaseOut);
+        //DEBUG: test the animation on start
+        playAnim(new Vector3(0, -500, -10), new Vector3(0,0,-10), 2f, AnimationType.EaseOut);
     }
-    public void playAnim(Vector2 from, Vector2 to, float duration, AnimationType animType)
+    public void playAnim(Vector3 from, Vector3 to, float duration, AnimationType animType)
     {
         //without using DOTween or any other tweening library but using coroutines
         StartCoroutine(Animate(from, to, duration, animType));
 
     }
 
-    private System.Collections.IEnumerator Animate(Vector2 from, Vector2 to, float duration, AnimationType animType)
+    private System.Collections.IEnumerator Animate(Vector3 from, Vector3 to, float duration, AnimationType animType)
     {
         float elapsed = 0f;
         while (elapsed < duration)
@@ -21,20 +22,20 @@ public class simpleAnimator : MonoBehaviour
             switch(animType)
             {
                 case AnimationType.EaseIn:
-                    transform.position = Vector2.Lerp(from, to, Mathf.Pow(elapsed / duration, 2));
+                    transform.position = Vector3.Lerp(from, to, Mathf.Pow(elapsed / duration, 2));
                     break;
                 case AnimationType.EaseOut:
-                    transform.position = Vector2.Lerp(from, to, 1 - Mathf.Pow(1 - (elapsed / duration), 2));
+                    transform.position = Vector3.Lerp(from, to, 1 - Mathf.Pow(1 - (elapsed / duration), 2));
                     break;
                 case AnimationType.EaseInOut:
                     float t = elapsed / duration;
                     if (t < 0.5f)
                     {
-                        transform.position = Vector2.Lerp(from, to, 2 * t * t);
+                        transform.position = Vector3.Lerp(from, to, 2 * t * t);
                     }
                     else
                     {
-                        transform.position = Vector2.Lerp(from, to, -1 + (4 - 2 * t) * t);
+                        transform.position = Vector3.Lerp(from, to, -1 + (4 - 2 * t) * t);
                     }
                     break;
             }
