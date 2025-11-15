f9ab1e7a6e54a75da9b3e6f612c78ada49eb2d7b
safetymax <31389735+safetymax@users.noreply.github.com>
2025-11-15 21:24:08 +0100

    clipboard

---
+++ Assets/Scripts/UI/clipboardAnim.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class clipboardAnim : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void animrein()
+    {
+            //get simple animator component
+            simpleAnimator animator = GetComponent<simpleAnimator>();
+            //play animation from current position to (0,0,-10) in 2 seconds with ease out animation type
+            animator.playAnim(transform.position, new Vector3(0, 0, -10), 2f, AnimationType.EaseOut);
+        
+    }
+
+    public void animraus()
+    {
+            //get simple animator component
+            simpleAnimator animator = GetComponent<simpleAnimator>();
+            //play animation from current position to (1500,354,-10) in 2 seconds with ease in animation type
+            animator.playAnim(transform.position, new Vector3(0, -328, -10), 2f, AnimationType.EaseIn);
+        
+    }
+}
